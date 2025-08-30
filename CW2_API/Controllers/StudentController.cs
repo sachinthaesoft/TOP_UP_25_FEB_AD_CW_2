6c928d7e112af1d43ba0390b1b682bc15edfa0d0
sachinthaesoft <sachintha.esoft@gmail.com>
2025-08-30 15:05:39 +0530

    create API

---
+++ CW2_API/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CW2_API.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<String> Get()
+        {
+            return ["One", "Two", "Three"];
+        }
+
+        [HttpPatch]
+        public IEnumerable<String> Patch()
+        {
+            return ["One", "Two", "Three"];
+        }
+
+        [HttpPut]
+        public IEnumerable<String> Put()
+        {
+            return ["One", "Two", "Three"];
+        }
+
+        [HttpDelete]
+        public IEnumerable<String> Delete()
+        {
+            return ["One", "Two", "Three"];
+        }
+
+        [HttpPost]
+        public IEnumerable<String> Post()
+        {
+            return ["One", "Two", "Three"];
+        }
+    }
+}
